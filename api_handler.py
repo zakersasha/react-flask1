5a29bd239873d4108e9a3b02360d723d823ecacf
Sasha <49103596+zakersasha@users.noreply.github.com>
2021-08-23 14:46:57 +0500

    Add files via upload

---
+++ api_handler.py
@@ -0,0 +1,32 @@
+from flask_restful import Resource, reqparse
+
+
+class HelloApiHandler(Resource):
+    def get(self):
+        return {
+            'resultStatus': 'SUCCESS',
+            'message': "Hello Api Handler"
+        }
+
+    def post(self):
+        print(self)
+        parser = reqparse.RequestParser()
+        parser.add_argument('type', type=str)
+        parser.add_argument('message', type=str)
+
+        args = parser.parse_args()
+
+        print(args)
+
+        request_json = args['message']
+
+        ret_msg = request_json
+
+        if ret_msg:
+            message = "Your Message Requested: {}".format(ret_msg)
+        else:
+            message = "No Msg"
+
+        final_ret = {"status": "Success", "message": message}
+
+        return final_ret
